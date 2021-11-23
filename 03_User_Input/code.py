6d64a5c70f313874f31ddc7f1d2d99938b5c02cd
emert117 <emremert117@gmail.com>
2021-11-23 12:24:58 +0300

    user input 2

--- 03_User_Input/code.py
+++ 03_User_Input/code.py
@@ -1,2 +1,7 @@
-name = input("Your name: ")
-print(name)
+# name = input("Your name: ")
+# print(name)
+
+size_input = input("How big is your house (in square feet): ")
+square_feet = int(size_input)
+square_meters = square_feet / 10.8
+print(f"{square_meters:.2f} 500m2")
