7460ac2bb83f75c5f1383cee3ddc60be05afcab8
emert117 <emremert117@gmail.com>
2021-11-28 19:29:06 +0300

    booleans

--- 06_advanced_set/code.py
+++ 06_advanced_set/code.py
@@ -3,3 +3,5 @@ abroad = {"Bob", "Anne"}
 
 local_friends = friends.difference(friends)
 print(friends)
+
+# union difference intersection
---
+++ 07_booleans/code.py
@@ -0,0 +1,6 @@
+print(5 == 5)
+
+friends = ["Rolf", "Bob"]
+abroad = ["Rolf", "Bob"]
+print(friends == abroad)
+print(friends is abroad) # false
