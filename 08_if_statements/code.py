7b4eaef57ab248ed2e3f445bdf8ce90fb0b5be77
emert117 <emremert117@gmail.com>
2021-11-28 21:08:32 +0300

    if statement

---
+++ 08_if_statements/code.py
@@ -0,0 +1,12 @@
+day_of_week = input("What day is it today?").lower()
+print(day_of_week == "Monday")
+
+if day_of_week == "monday":
+    print("Monday is great")
+    print("a good start")
+elif day_of_week == "tuesday":
+    print("It is tuesday")
+else:
+    print("some other day")
+
+print("This alwasys runs")
