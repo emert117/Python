f67362a89b9d0c82c1146e1ca51e5b75eab56e65
emert117 <emremert117@gmail.com>
2021-12-04 12:37:59 +0300

    dictionary usage 2

--- 13_dictionaries/code.py
+++ 13_dictionaries/code.py
@@ -2,6 +2,11 @@ friend_ages = {"Emre": 34, "Hatice": 30, "Arda": 15, "Efe": "-"}
 friend_ages["Eymen"] = 10 # doesn't exist in dictionary
 friend_ages["Emre"] = 40
 print(friend_ages)
+for friend in friend_ages:
+    print(f"{friend}: {friend_ages[friend]}") # key access
+
+for name, age in friend_ages.items(): # destructing
+    print(f"{name}: age: {age}")
 
 friends = [
     {"name": "Emre", "age": 34},
@@ -9,3 +14,5 @@ friends = [
     {"name": "Efe", "age": 10},    
 ]
 print(friends[1]["name"])
+
+# in keyword 
