809f12ca06d1ced4a4affe00e8d145bacdd2c394
emert117 <emremert117@gmail.com>
2021-12-29 20:56:28 +0300

    review codes, nothing much

--- 23_object_oriented_programming/code.py
+++ 23_object_oriented_programming/code.py
@@ -1,11 +1,12 @@
 class Student:
-    def __init__(self, name):
+    def __init__(self, name, lastname):
         self.name = name
+        self.lastname = lastname
         self.grades = (1, 2, 3, 4)
     
     def average(self):
         return sum(self.grades) / len(self.grades)
 
-student = Student("emre")
+student = Student("emre", "mert")
 print(Student.average(student))
 print(student.average())
