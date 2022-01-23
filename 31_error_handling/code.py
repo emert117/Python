1698d123c7a81ea2c3d474b2ba70d5d98901884a
emert117 <emremert117@gmail.com>
2022-01-23 17:07:18 +0300

    error handling 2

--- 31_error_handling/code.py
+++ 31_error_handling/code.py
@@ -1,4 +1,8 @@
+from lib2to3.pytree import Base
+
+
 def divide(dividend, divisor):
+    raise BaseException("I am bored")
     if divisor == 0:
         raise ZeroDivisionError("Divisor cannot be 0 (zero)")
 
@@ -7,6 +11,12 @@ def divide(dividend, divisor):
 grades = []
 try:
     average = divide(sum(grades) / len(grades))
-    print(f"The average is {average}")
 except ZeroDivisionError as e:
     print("There are no grades in your list")
+except BaseException as e:
+    print(f"Unknown error", e)
+else:
+    print(f"The average is {average}") # run if there ia no errors
+finally:
+    print("The end") # always runs
+
