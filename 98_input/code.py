46b47b312b9c65c8772fa73c528ae58c7f568075
emert117 <emremert117@gmail.com>
2022-02-10 11:29:32 +0300

    pipe output to input

--- 98_input/code.py
+++ 98_input/code.py
@@ -4,16 +4,30 @@ import os
 import random
 import re
 import sys
+from subprocess import Popen, PIPE, TimeoutExpired
 
 
 
 if __name__ == '__main__':
-    print("Enter a number: ", end=' ')
-    n = int(input().strip())
-    if n % 2 == 1:
-        print("odd")
-    else:
-        print("even")
 
-    second_input = str(input())
-    print(f"second input: {second_input}")
+    test_inputs = []
+
+    p1 = Popen(["printf", "55\nmert"], stdout=PIPE)
+    p1.wait()
+    process = Popen(["python3", "-c", "print('first input: ' + input())\nprint('second input: ' + input())"], stdin=p1.stdout, stdout=PIPE)
+
+    # process = Popen(["printf '55mert'", "|", "python3", "-c", f"import sys\n\rimport traceback\n\rtry:\n\r    print('printing input from user')\n\r    print(input())\n\rexcept Exception as e:\n\r    exception_type, exception_object, exception_traceback = sys.exc_info()\n\r    line_number = exception_traceback.tb_lineno\n\r    print(exception_type, line_number, str(e), end ='')"]
+    #        + test_inputs, stdout=PIPE)
+
+    try:
+        (output, err) = process.communicate(timeout=10)
+        exit_code = process.wait()
+        print(f"output: {output}")
+        print(f"exit code: {exit_code}")
+    except TimeoutExpired as timeout_err:
+        error_message = "Timeout"
+        print(error_message)
+    except Exception as e:
+        # Main Exception
+        # abort test runs
+        print(str(e))
---
+++ 98_input/code2.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    print("Enter first input: ", end=' ')
+    first_input = input()
+    print(f"first input: {first_input}")
+
+    print("Enter second input: ", end=' ')
+    second_input = str(input())
+    print(f"second input: {second_input}")
